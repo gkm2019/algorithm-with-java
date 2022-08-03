60509e8a2d4107b2706c697e3cf7e84dbc82210c
popo2019@naver.com <kyeongmin1@kyeongminui-MacBookPro.local>
2022-08-03 22:28:02 +0900

    create class 베스트앨범

---
+++ src/programmers/베스트앨범.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 베스트앨범 {
+    public static void main(String[] args) {
+
+        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = {500, 600, 150, 800, 2500};
+
+        int[] ans = Solution.solution(genres, plays);
+
+        System.out.println(ans.length);
+    }
+
+    class Solution {
+        public static int[] solution(String[] genres, int[] plays) {
+            int[] answer = {};
+            return answer;
+        }
+    }
+}
