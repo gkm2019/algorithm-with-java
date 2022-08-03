18f3898e5bd723ba1278f9dd64ad23d714f509f0
popo2019@naver.com <kyeongmin1@kyeongminui-MacBookPro.local>
2022-08-03 19:54:55 +0900

    위장

--- src/programmers/위장.java
+++ src/programmers/위장.java
@@ -1,4 +1,41 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class 위장 {
+
+    public static void main(String[] args) {
+
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+
+        int ans = Solution.solution(clothes);
+
+        System.out.println(ans);
+    }
+
+    class Solution {
+        public static int solution(String[][] clothes) {
+            int answer = 1;
+            Map<String, Integer> map = new HashMap<>();
+            for(String[] strs : clothes) {
+                String key = strs[1];
+
+                if(map.containsKey(key)) {
+                    int value = map.get(key);
+                    map.put(key, value + 1);
+                } else {
+                    map.put(key, 1);
+                }
+            }
+
+            for(Map.Entry<String, Integer> entry : map.entrySet()) {
+                int tmp = entry.getValue() + 1;
+                answer *= tmp;
+            }
+            return answer -1;
+        }
+    }
 }
