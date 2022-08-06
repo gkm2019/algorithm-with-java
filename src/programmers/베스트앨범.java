55134cbf47f622e6f2903da5a25d40ec9a596b3c
popo2019@naver.com <kyeongmin1@kyeongminui-MacBookPro.local>
2022-08-06 09:37:58 +0900

    베스트엘범 클래스 추가

--- src/programmers/베스트앨범.java
+++ src/programmers/베스트앨범.java
@@ -29,29 +29,6 @@ public class 베스트앨범 {
     class Solution {
         public static int[] solution(String[] genres, int[] plays) {
             int[] answer = {};
-            Map<String, Integer> sortTotalMap = new HashMap<>();
-            List<AlbumDTO> albumDTOs = new ArrayList<>();
-            //장르별 total 재생 횟수
-            for(int i=0; i<genres.length; i++) {
-                String key = genres[i];
-                int value = plays[i];
-                if(sortTotalMap.containsKey(key)) {
-                    value += sortTotalMap.get(key);
-                }
-
-                sortTotalMap.put(key, value);
-                AlbumDTO dto = new AlbumDTO(i, key, plays[i], 0);
-                albumDTOs.add(dto);
-            }
-
-            //total count setting
-            for(AlbumDTO dto : albumDTOs) {
-                int total = sortTotalMap.get(dto.genre);
-                dto.total = total;
-            }
-
-            //장르 total counting
-            //TreeMap<String, List<AlbumDTO>> result = albumDTOs.stream()
             return answer;
         }
     }
